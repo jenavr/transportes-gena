0ff44d0671f1a398fdde7546dfb27d3161210164
Genaro Ochoa <genaro.ochoa@lifestance.com>
2026-05-14 13:04:37 -0600

    use relative backend url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,5 @@
 export const BACKEND_URL =
-  (import.meta.env.VITE_BACKEND_URL as string | undefined) ??
-  'http://localhost:4000';
+  (import.meta.env.VITE_BACKEND_URL as string | undefined) ?? '';
 
 export const apiUrl = (path: string) =>
   `${BACKEND_URL}${path.startsWith('/') ? path : `/${path}`}`;
